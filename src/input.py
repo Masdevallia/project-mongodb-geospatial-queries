e24d478391cdda1f4dc38b801bb7e27a79569d56
Cristina Rota <cristina.rota.m@gmail.com>
2019-11-19 10:08:04 +0100

    Adding functions

--- src/input.py
+++ src/input.py
@@ -1,24 +1,26 @@
 
+from colorama import init
+init()
+from colorama import Fore, Back, Style
 
 def title():
     print('''
-    ---------------------------------------------------------------------------------
-                        FIND THE PERFECT LOCATION FOR YOUR COMPANY              
-    ---------------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------------------------
+                                FIND THE PERFECT LOCATION FOR YOUR COMPANY              
+    ----------------------------------------------------------------------------------------------------
     ''')
 
 
 def input_money():
     print('''
 
-
-    ---------------------------------------------------------------------------------
-        Developers like to be near successful tech startups that have raised at
-        least (n) dollars.
+    
+        Developers like to be near successful tech startups that have raised at least (n) dollars.
+    ----------------------------------------------------------------------------------------------------
         What amount of money should they have raised?
         Please enter a valid integer, without thousands separator.
         Example: For 1 million dollars, enter: 1000000
-    ---------------------------------------------------------------------------------
+
     ''')
 
 
@@ -26,13 +28,12 @@ def input_year():
     print('''
 
 
-    ---------------------------------------------------------------------------------
-        Nobody in the company likes to have companies with more than (n) years in
-        a radius of 2 KM.
-        How old can these companies be?
+        Nobody in the company likes to have companies with more than (n) years in a radius of 2 KM.
+    ----------------------------------------------------------------------------------------------------
+        How old can these companies be at most?
         Please enter a valid integer.
         Example: For 10 years, enter: 10
-    ---------------------------------------------------------------------------------
+    
     ''')
 
 
@@ -40,10 +41,9 @@ def input_starbucks():
     print('''
 
 
-    ---------------------------------------------------------------------------------
-        Executives like Starbucks A LOT. Ensure there's a Starbucks not to far.
-    ---------------------------------------------------------------------------------
-                  Please wait. We are collecting some information...
+                    Executives like Starbucks A LOT. Ensure there's a Starbucks not to far.
+    ----------------------------------------------------------------------------------------------------
+                            Please wait. We are collecting some information...
     ''')
 
 
@@ -51,10 +51,9 @@ def input_vegan():
     print('''
 
 
-    ---------------------------------------------------------------------------------
-                                  The CEO is Vegan.
-    ---------------------------------------------------------------------------------
-                  Please wait. We are collecting some information...
+                                        The CEO is Vegan.
+    ----------------------------------------------------------------------------------------------------
+                            Please wait. We are collecting some information...
     ''')
 
 
@@ -62,11 +61,9 @@ def input_party():
     print('''
 
 
-    ---------------------------------------------------------------------------------
-              All people in the company have between 25 and 40 years, 
-                      give them some place to go to party.
-    ---------------------------------------------------------------------------------
-                  Please wait. We are collecting some information...
+        All people in the company have between 25 and 40 years, give them some place to go to party.
+    ----------------------------------------------------------------------------------------------------
+                            Please wait. We are collecting some information...
     ''')
 
 
@@ -74,10 +71,9 @@ def input_airport():
     print('''
 
 
-    ---------------------------------------------------------------------------------
-                         Account managers need to travel a lot.
-    ---------------------------------------------------------------------------------
-                  Please wait. We are collecting some information...
+                                Account managers need to travel a lot.
+    ----------------------------------------------------------------------------------------------------
+                            Please wait. We are collecting some information...
     ''')
 
 
@@ -85,9 +81,8 @@ def input_school():
     print('''
 
     
-    ---------------------------------------------------------------------------------
-                      30% of the company have at least 1 child.
-    ---------------------------------------------------------------------------------
-                  Please wait. We are collecting some information...
+                                30% of the company have at least 1 child.
+    ----------------------------------------------------------------------------------------------------
+                            Please wait. We are collecting some information...
     ''')
 
